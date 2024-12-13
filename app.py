2169f6c4f7de7fd03267eac2a96b8be91d087ae2
nikhil <nikhilpokuri143@gmail.com>
2024-12-13 12:53:59 +0530

    day2

--- app.py
+++ app.py
@@ -17,6 +17,14 @@ table_query = """
     """
 my_cursor.execute(table_query)
 
+book_table_query = """ 
+        create table if not exists bookings (bookind_id int primary key auto_increment,
+        booking_date date, seats_req int, contanct_email varchar(50)
+        );
+    """
+my_cursor.execute(book_table_query)
+
+
 @app.route("/", methods=["GET"])
 def homepage():
     return render_template("index.html")
@@ -39,6 +47,50 @@ def register_event():
         my_cursor.execute(insert_query, values)
         my_connection.commit()
         return "sample"
-    return render_template("register_event.html")
+    else:
+        return render_template("register_event.html")
+
+@app.route("/view_events", methods=["GET"])
+def view_events():
+    read_query = """ 
+        select * from events;
+    """
+    my_cursor.execute(read_query)
+    raw = my_cursor.fetchall()
+    print(raw)
+    return render_template("view_events.html", output=raw)
+
+@app.route("/book_event", methods=["GET", "POST"])
+def book_event():
+    if request.method == "POST":
+        event_id = int(request.form["event_id"])
+        seats_req = int(request.form["seats"])
+        booking_date = request.form["booking_date"]
+        contact_email = request.form["contact_email"]
+        
+        get_event_query = """ 
+            select max_seats from events where event_id = %s;
+        """
+        values = [event_id]
+        my_cursor.execute(get_event_query, values)
+        fetched = my_cursor.fetchall()
+
+        if not fetched:
+            return f"Invalid EventID {event_id}"
+        else:
+            max_seats = fetched[0][0]
+            if seats_req > max_seats:
+                return f"Cannot book more than {max_seats}"
+            else:
+                insert_query = """ 
+                    insert into bookings (booking_date, seats_req, contact_email)
+                    values (%s, %s, %s);
+                """
+                values = [booking_date, seats_req, contact_email]
+                my_cursor.execute(insert_query, values)
+                my_connection.commit()
+                return "Booking Succesfull"
+    else:
+        return render_template("book_event.html")
 
 app.run(debug=True)
